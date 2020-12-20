3325adf78b4a95a3936f7d1e1d819efd07c5b4cd
Corsin Camichel <cocaman@gmail.com>
2020-12-20 16:41:03 +0100

    adding option to have Yara rules running daily

--- yara_scan_upload.py
+++ yara_scan_upload.py
@@ -15,14 +15,21 @@ API_KEY = ""
 parser = argparse.ArgumentParser(description='Upload a Yara rule to be scanned on Yara Scan Service')
 parser.add_argument('-f', '--file', help='Yara to upload (required)', type=str, metavar="FILE", required=True, nargs=argparse.ONE_OR_MORE)
 parser.add_argument('-a', '--apikey', help='Your personal API key', type=str, metavar="apikey", required=False, default=API_KEY)
+parser.add_argument('-d', '--daily', help='Run this rule daily', dest="daily", required=False, default=False, action='store_true')
+parser.set_defaults(feature=True)
 args = parser.parse_args()
 
 headers = {'APIKEY': args.apikey}
+data = {}
+
 files = []
 for f in args.file:
     files.append(('file[]', open(f,'rb')))
 
-response = requests.post('https://riskmitigation.ch/yara-scan/api/', files=files, headers=headers)
+if(args.daily):
+    data = {"daily" : "true"}
+
+response = requests.post('https://riskmitigation.ch/yara-scan/api/', files=files, headers=headers, data=data)
 json_data = json.loads(response.text)
 status = json_data['status']
 
